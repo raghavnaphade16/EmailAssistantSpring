51d9beaeb7c07b9d1cda332aca88de7b04cb081e
Raghav <ragnap99@gmail.com>
2025-07-09 20:14:28 +0530

    Project Setup

---
+++ email-writer-rn/email-writer-rn/src/main/java/com/emailassistant/email_writer_rn/EmailWriterRnApplication.java
@@ -0,0 +1,13 @@
+package com.emailassistant.email_writer_rn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailWriterRnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailWriterRnApplication.class, args);
+	}
+
+}
---
+++ email-writer-rn/email-writer-rn/src/test/java/com/emailassistant/email_writer_rn/EmailWriterRnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emailassistant.email_writer_rn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailWriterRnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
