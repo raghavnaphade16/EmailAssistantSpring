a5bd56861945e3a045ceefc32a52a09fc5ba0287
Raghav <ragnap99@gmail.com>
2025-07-18 09:07:18 +0530

    wip

---
+++ todo-app/src/main/java/com/raghav/todo_app/ToDoController.java
@@ -0,0 +1,11 @@
+package com.raghav.todo_app;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/todo")
+public class ToDoController {
+
+}
---
+++ todo-app/src/main/java/com/raghav/todo_app/TodoService.java
@@ -0,0 +1,4 @@
+package com.raghav.todo_app;
+
+public class TodoService {
+}
